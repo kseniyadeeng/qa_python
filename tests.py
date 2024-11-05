73c4972c5cf87ce6789d7e49d6f262cc7f75fae8
Kseniya Kovalenko <kkovalenko051@gmail.com>
2024-11-05 06:46:56 +0300

    new_commit

--- tests.py
+++ tests.py
@@ -4,29 +4,14 @@ from main import BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        collector.add_new_book('Ведьмак')
+        collector.add_new_book('Кристина')
+        assert collector.get_books_genre() == {'Ведьмак': '',
+                                               'Кристина': ''}
 
     #add_new_book
-    def test_length_name_book_from_1_to_40_not_empty_result(self, name_book):
-        collector = BooksCollector()
-        assert collector.add_new_book(name_book) != ""
 
     def test_add_new_book_without_name_null_result(self, collector):
         collector.add_new_book("")
@@ -64,7 +49,9 @@ class TestBooksCollector:
 
     # get_books_genre()
     def test_get_books_genre_from_nonexist_books_null_result(self, collector):
-        assert collector.get_books_genre() == {}
+        collector.add_new_book("Гарри Поттер")
+        collector.set_book_genre("Гарри Поттер", "Ужасы")
+        assert collector.get_books_genre("Атака Титанов") == ""
 
     # get_books_for_children()
     def test_get_books_for_children_exist_(self, collector):
